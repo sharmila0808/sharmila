5423a1c0f072a2fd5bb0b8bd57d4615a0b5e4576
sharmila0808 <31504272+sharmila0808@users.noreply.github.com>
2017-08-31 15:16:54 +0530

    Create xy.c

---
+++ xy.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("enter a num");
+scanf("%d",&i);
+if(i<0)
+{
+printf("\n it is a negative num");
+}
+else if(i>0)
+{
+printf("\n it is a positive num");
+}
+else if(i==0)
+{
+printf("\n the number is zero")'
+}
+else
+{
+printf("\n invalid");
+}
+}
