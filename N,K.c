cb6972086d053c89eb94b52139def877ba8b0ee8
sharmila0808 <31504272+sharmila0808@users.noreply.github.com>
2018-01-11 16:53:27 +0530

    Create N,K.c

---
+++ N,K.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int N,K,a[N],i;
+clrscr();
+printf("enter 2 numbers");
+scanf("%d %d",&N,&K);
+printf("enter array of N numbers");
+scanf("%d",&a[]);
+if(N>=K)
+{
+for(i=0;i<=K;i++)
+{
+sum=sum+a[i];
+}
+}
+printf("%d",sum);
+getch();
+}
